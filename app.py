8fb948691917a26fd23fc66f4b6101742ccd4e70
edwardngo <hoangso8000@gmail.com>
2021-05-15 21:07:14 +0700

    Add: api to neural network

--- app.py
+++ app.py
@@ -3,25 +3,37 @@ import pickle
 import os
 import glob
 from tqdm import tqdm
+from flask_cors import CORS
 import pickle
 from underthesea import word_tokenize
 import gensim
 from sklearn.decomposition import TruncatedSVD
-
+import numpy as np
+from keras.models import load_model
 MODEL = "model"
 MODEL_PATH = "model/svm.pkl"
 WORD_EMBEDDING = "model/tfidf.pkl"
 DATA_TRANSFORM = "model/tfidfdata.pkl"
+NN_PATH = "model/model.h5"
+ENCODER = "model/encoder.pkl"
 SVD = "model/svd_data.pkl"
 clf = pickle.load(open(MODEL_PATH,"rb"))
 tfidf_vect = pickle.load(open(WORD_EMBEDDING,"rb"))
 tfidf_data = pickle.load(open(DATA_TRANSFORM,"rb"))
+encoder = pickle.load(open(ENCODER,"rb"))
 # svd = TruncatedSVD(n_components=300, random_state=42)
 # svd.fit(tfidf_data)
 svd = pickle.load(open(SVD,"rb"))
 
 app = Flask(__name__)
+CORS(app)
 
+# def inference_nn(text):
+#   print(text)
+#   text = tfidf_vect.transform([text])
+#   text = svd.transform(text)
+
+  
 def preprocess(document):
     document = ' '.join(gensim.utils.simple_preprocess(document))
     document = word_tokenize(document, format="text")
@@ -55,7 +67,7 @@ def init():
         trained_models = os.listdir(MODEL)
         # trained_models = ["model/"+fname for fname in trained_models]
         response = jsonify(trained_model=tuple(trained_models))
-        response.headers.add("Access-Control-Allow-Origin", "*")
+        # response.headers.add("Access-Control-Allow-Origin", "*")
 
         return response
     except:
@@ -63,22 +75,29 @@ def init():
 
 @app.route('/classification/predict/<model>',methods=["POST"])
 def handle_predict_request(model):
-    clf = pickle.load(open("model/"+model,"rb"))
+    if model != "neural-net":
+        clf = pickle.load(open("model/"+model +".pkl","rb"))
+    else:
+        clf = load_model("model/model.h5")
+
     requested_data = request.get_json()
     input = requested_data['input']
-    # f = open(TEST_FILE_PATH, "w",encoding='utf-16-le')
-    # f.write(input)
-    # f.close()
-    # pred = Predictor()
-    if "nb.pkl" in model:
+
+    if "nb" in model:
         input_vect = preprocess_nb(input)
     else:
         input_vect = preprocess(input)
     label = clf.predict(input_vect)
-    try:
-        return label[0]
-    except:
-        return "500"
+
+    if model != "neural-net":
+        response = label[0]
+    else:
+        label_index = np.argmax(np.squeeze(label,axis = 0))
+        response =encoder.inverse_transform([label_index])[0]
+
+    # response = response.headers.add("Access-Control-Allow-Origin", "*")
+    print(response)
+    return response
 
 if __name__ == '__main__':
 #     test = """
